cf7100b3aee7c2ef2df774e90e03d47d968e21fb
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-02-11 14:57:19 +0100

    fix: use current host as proxy endpoitn

--- web/index.tsx
+++ web/index.tsx
@@ -5,7 +5,7 @@ import { Map } from './Map/Map'
 ReactDOM.render(
 	<Map
 		proxyEndpoint={(
-			process.env.PROXY_ENDPOINT || 'http://127.0.0.1:8080/'
+			process.env.PROXY_ENDPOINT || document.location.href
 		).replace(/\/+$/, '')}
 	/>,
 	document.getElementById('map'),
