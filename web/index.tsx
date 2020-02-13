af8993338b24868702c3743957bf8e52a7c78cd7
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-02-13 12:38:54 +0100

    docs: add source link to website

--- web/index.tsx
+++ web/index.tsx
@@ -2,6 +2,13 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Map } from './Map/Map'
 
+console.log(
+	'%c ThingyWorld ',
+	'background-color: #00a9ce; color: #ffffff; padding: 0.25rem;',
+	'Source code:',
+	'https://github.com/coderbyheart/nrfcloud-udp-proxy',
+)
+
 ReactDOM.render(
 	<Map
 		proxyEndpoint={(
